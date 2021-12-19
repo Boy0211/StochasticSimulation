65390f62bef8725853802d859ad020763e6e0662
S Kuilboer <s.kuilboer@gmail.com>
2021-12-19 14:26:18 +0100

    Added two functions for drawing and saving if needed

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -58,6 +58,40 @@ def create_map(data, type, data_tour):
 
     return map
 
+def draw_2y_figure(fitnesses, temps, Nmax, save=False, filename=None):
+
+    fig, axis = plt.subplots(figsize=(12, 8))
+    axis.plot(list(range(Nmax)), fitnesses, color='blue', alpha=0.5)
+
+    axis.set_xlabel('Iterations', fontsize=14)
+    axis.set_ylabel('Fitnesses', fontsize=14)
+
+    axis2 = axis.twinx()
+
+    axis2.plot(list(range(Nmax)), temps, color='red', alpha=0.5)
+    axis2.set_ylabel('Temps', fontsize=14)
+
+    fig.suptitle('Fitness and temp over iterations', fontsize=16)
+    plt.legend()
+
+    if (save == True) & (filename != None):
+        fig.savefig(f'overleaf/figures/{filename}.png')
+    else:
+        plt.show()
+
+def draw_route(map, save=False, filename=None):
+
+    fig, axis = plt.subplots(figsize=(12, 8))
+
+    axis.scatter(map.coords[:,0], map.coords[:,1], marker='s')
+    axis.plot(map.coords[:,0], map.coords[:,1], alpha=0.5)
+    fig.suptitle('The route of the travelling salesman')
+
+    if (save == True) & (filename != None):
+        fig.savefig(f'../overleaf/figures/{filename}.png')
+    else:
+        plt.show()
+
 
 # data file types
 types = ["a280", "eil51", "pcb442"]
@@ -71,8 +105,8 @@ map = create_map(data, type, data_tour)
 
 # anneal
 T0 =50
-Nmax = 100000
-sched = 3
+Nmax = 40000
+sched = 2
 B = 1
 simAnneal = SimAnneal(map, T0, Nmax, sched, B)
 
@@ -81,9 +115,8 @@ temps = simAnneal.temps
 print(temps[-1])
 print(fitnesses[-1])
 #
-plt.plot(list(range(Nmax)), fitnesses)
-plt.plot(list(range(Nmax)), temps)
-plt.show()
+draw_2y_figure(fitnesses, temps, Nmax)
+draw_route(map)
 
 # # tests the swap function
 inds = [50,3]
@@ -93,9 +126,4 @@ i = 10
 # map.swap_1node([i,j])
 #
 #
-# print(map.nodes)
-# print(map.nodes_list)
-# print(len(map.nodes))
-# plt.scatter(map.coords[:,0], map.coords[:,1])
-# plt.plot(map.coords[:,0], map.coords[:,1])
-# # plt.show()
+
